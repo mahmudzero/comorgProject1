5f94b098a600b2ec663cd08b93c921c638025071
Mahmud Ahmad <mahmudahmad@Mahmuds-MacBook-Pro.local>
2017-01-11 19:58:03 -0500

    Initial Commit

---
+++ testingCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testingCPP
+//
+//  Created by Mahmud Ahmad on 1/11/17.
+//  Copyright © 2017 Mahmud Ahmad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
